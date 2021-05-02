77233c4a8150f6dd0c533d839192a0c9d2f7f20f
Ramon Gonçalves <raigons@gmail.com>
2021-05-02 17:54:47 -0300

    Documentação

--- lib/toggle_example_web/controllers/toggle_controller.ex
+++ lib/toggle_example_web/controllers/toggle_controller.ex
@@ -7,33 +7,38 @@ defmodule ToggleExampleWeb.ToggleController do
   alias ToggleExample.Toggles
   alias ToggleExample.Toggles.Toggle
 
-  # num ambiente de produção o deploy seria necessário
-  # mas para um ambiente de dev pode ser ok
-  # contudo para ambiente de testes não vai ser ideal
+  @doc """
+  num ambiente de produção o deploy seria necessário
+  mas para um ambiente de dev pode ser ok
+  contudo para ambiente de testes não vai ser ideal
+  """
   def hardcoded(conn, _params) do
     router_toggle(:hard, t_HARDCODED(), conn)
   end
 
-  # necessita de variável de ambiente no momento de
-  # executar o servidor. Isso significa que uma mudança
-  # de toggle necessita de um deploy do sistema
-  # para testes seria necessário criar um stub para
-  # o aplication.get_env em cada cenário, sendo possível
-  # ter dinamicidade
+  @doc """
+  necessita de variável de ambiente no momento de
+  executar o servidor. Isso significa que uma mudança
+  de toggle necessita de um deploy do sistema
+  para testes seria necessário criar um stub para
+  o aplication.get_env em cada cenário, sendo possível ter dinamicidade
+  """
   def env_var(conn, _params) do
     router_toggle(:env, env_var_1(), conn)
   end
 
-  #necessita de uma fonte externa para gerenciar as
-  #toggles. Traz o benefício de não precisar de deploy
-  #para alteração do valor.
-  #para teste apenas necessário fazer stub da fonte de toggles
+  @doc """
+  necessita de uma fonte externa para gerenciar as
+  toggles. Traz o benefício de não precisar de deploy
+  para alteração do valor.
+  para teste apenas necessário fazer stub da fonte de toggles
+  """
   def external_service(conn, _params) do
-    #o nome da toggle pode ser um arquivo separado
+    # o nome da toggle pode ser um arquivo separado
     router_toggle(:external, external_service_toggle("TOGGLE_1"), conn)
   end
 
-  #da mesma forma que de serviço externo, não necessita de deploy
+  # da mesma forma que de serviço externo, não necessita de deploy
   def database(conn, _params) do
     with %Toggle{value: value} <- Toggles.get_toggle_by_name("TOGGLE_1") do
       router_toggle(:database, value, conn)
@@ -42,13 +47,13 @@ defmodule ToggleExampleWeb.ToggleController do
     end
   end
 
-  #toggle true
+  # toggle true
   defp router_toggle(:hard, true, conn), do: response_toggle_on(conn)
   defp router_toggle(:env, true, conn), do: response_toggle_on(conn)
   defp router_toggle(:external, true, conn), do: response_toggle_on(conn)
   defp router_toggle(:database, true, conn), do: response_toggle_on(conn)
 
-  #toggle false
+  # toggle false
   defp router_toggle(:hard, false, conn), do: default_response(conn)
   defp router_toggle(:env, false, conn), do: default_response(conn)
   defp router_toggle(:external, false, conn), do: default_response(conn)
